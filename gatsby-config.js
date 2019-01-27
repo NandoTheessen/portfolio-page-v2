0eff79272b193eb176820bbed83e09e2b229cd61
NandoTheessen <nando.theessen@gmail.com>
2019-01-28 00:23:27 +0100

    Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ var sitePlugins = [
       background_color: '#663399',
       theme_color: '#663399',
       display: 'minimal-ui',
-      icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      icon: 'src/images/favicon-512x512.png', // This path is relative to the root of the site.
     },
   },
   'gatsby-plugin-offline',
